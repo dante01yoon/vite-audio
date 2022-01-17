b04670bb73f91563c4ac9db24fa6664ba17361e4
dante01yoon <bunggl@naver.com>
2022-01-18 01:17:22 +0900

    Update: first touch

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  esbuild: {
+    jsxInject: `import React from "react"`,
+  },
+  plugins: [react()]
+})
