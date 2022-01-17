5e4da243db29e612b4fb99c091859766bf44ebcf
dante01yoon <bunggl@naver.com>
2022-01-18 01:45:33 +0900

    Update: path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
-// https://vitejs.dev/config/
+/** 
+ * @see https://vitejs.dev/config/
+ */
 export default defineConfig({
   esbuild: {
     jsxInject: `import React from "react"`,
+  },
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src")},
+      { find: "@components", replacement: path.resolve(__dirname, "src/components")},
+      { find: "@pages", replacement: path.resolve(__dirname, "src/pages")},
+      { find: "@utils", replacement: path.resolve(__dirname, "src/utils")}
+    ]
   },
   plugins: [react()]
 })
