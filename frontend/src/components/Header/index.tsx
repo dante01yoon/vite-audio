b6f962e14e1efe06cfda6bd34b8b36b254b2bbba
stella-jo <0604seohee@gmail.com>
2022-02-15 17:40:19 +0900

    Add: Header component

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import { BackButton } from '@components/index';
+
+interface HeaderProps { }
+
+export const Header: FC<HeaderProps> = ({ ...props }) => {
+	return (
+		<header className='flex flex-col flex-nowrap gap-[36px] items-start p-[60px]'>
+			<BackButton />
+			<h1 className='font-bold text-[2rem]'>{props.children}</h1>
+		</header>
+	);
+};
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -5,3 +5,4 @@ export * from './Icon';
 export * from './Field';
 export * from './Container';
 export * from './Modal';
+export * from './Header';
