2a2f0ece335a56be14e5d590e7c89ce1c73d6107
stella-jo <0604seohee@gmail.com>
2022-02-16 02:35:02 +0900

    Update: Card component setup

--- frontend/src/components/Card/Learning.tsx
+++ frontend/src/components/Card/Learning.tsx
@@ -1,30 +1,26 @@
-import { FC } from 'react';
+import { FC, MouseEventHandler } from 'react';
 import cs from 'classnames';
 
 export interface LearningProps {
   title: string; // 카드 이름
   text: string; // 원문
+  clickHandler: MouseEventHandler;
   imgSrc?: string;
   toggle?: boolean;
 }
 
 const styleMap = {
-  bg: (imgSrc?: string) => ({ [`bg-url(${imgSrc})`]: true, 'rounded-[50%]': true }),
-  border: (toggle: boolean = false) => ({
-    'border-solid': toggle,
-    'border-none': !toggle,
-    'border-2': toggle,
-    'border-cyan-700': toggle
-  })
+  bg: (imgSrc?: string) => ({ [`bg-[url('${imgSrc}')]`]: true, 'rounded-[50%]': true }),
+  border: (toggle: boolean = false) => toggle ? 'border-cyan-700' : 'border-transparent'
 };
 
-export const Learning: FC<LearningProps> = ({ title, text, imgSrc, toggle }) => {
+export const Learning: FC<LearningProps> = ({ title, text, clickHandler, imgSrc, toggle }) => {
   return (
-    <div className={`flex cmp-learning-card ${styleMap.border(toggle)}`}>
-      <div className={cs(styleMap.bg(imgSrc))} />
-      <div>
-        <div className={'font-bold mb-[10px]'}>{title}</div>
-        <div className="text-ellipsis overflow-hidden f-width max-w-[200px] decoration-cardText">
+    <div onClick={clickHandler} className={`text-[0.75rem] p-[16px] flex items-center gap-[12px] cmp-learning-card rounded-[8px] border-[1px] border-solid ${styleMap.border(toggle)}`}>
+      <div className={`${cs(styleMap.bg(imgSrc))} w-[48px] aspect-square bg-cover bg-no-repeat`} />
+      <div className='flex flex-col flex-nowrap gap-[8px]'>
+        <div className={'font-bold'}>{title}</div>
+        <div className="text-ellipsis overflow-hidden decoration-cardText">
           {text}
         </div>
       </div>
