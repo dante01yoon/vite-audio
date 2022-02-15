1dc130259e085e94ede7a42870a5e527b143fcd8
stella-jo <0604seohee@gmail.com>
2022-02-15 17:38:01 +0900

    Add: BackButton component

---
+++ frontend/src/components/Buttons/BackButton.tsx
@@ -0,0 +1,12 @@
+import { FC, HTMLAttributes } from 'react';
+import { Icon } from '@components/index';
+
+interface BackButtonProps extends HTMLAttributes<HTMLButtonElement> { }
+
+export const BackButton: FC<BackButtonProps> = ({ ...props }) => {
+	return (
+		<button className='p-[12px]' {...props}>
+			<Icon iconType='arrow' width='12px' height='12px' />
+		</button>
+	);
+};
