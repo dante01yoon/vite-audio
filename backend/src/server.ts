4328931ec1ce6eaba9f198942ca944ed1de8e6ca
dante01yoon <bunggl@naver.com>
2022-02-26 15:01:48 +0900

    update: mongodb 연결

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./utils";
 import { translateRouter as translate, ttsRouter as tts } from "./routes";
 import "./utils/fetch-polyfills";
+import { authChecker } from "./middleware";
 
 dotenv.config({
   path: "./.env",
@@ -14,11 +15,9 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use("/translate", translate);
-app.use("/tts", tts);
-connectDB().then(() => {
-  console.log("db is connected");
-});
+app.use("/translate", authChecker, translate);
+app.use("/tts", authChecker, tts);
+connectDB(() => console.log("db is connected"));
 app.listen(PORT, () => {
   console.log(`app is running on PORT ${PORT}`);
 });
--- backend/src/utils/connectDB.ts
+++ backend/src/utils/connectDB.ts
@@ -1,5 +1,23 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
+export const connectDB = async (
+  successCB?: () => void,
+  failCB?: () => void
+) => {
+  const db = mongoose.connection;
+  db.on("connecting", () => {
+    console.log("connecting mongoose...");
+  });
+  db.on("connected", () => {
+    if (successCB) {
+      successCB();
+    }
+  });
+  db.on("error", () => {
+    console.error.bind(console, "MongoDB connection error");
+    if (failCB) {
+      failCB();
+    }
+  });
   await mongoose.connect(process.env.DB_CONNECT_URL);
 };
